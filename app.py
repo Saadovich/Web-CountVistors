47ded1e5bae0a2b2c0f03f09473af852e30bdef4
Saadovich <A.Saadovich@hotmail.com>
2024-02-04 07:28:26 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,24 @@
+import time
+
+import redis
+from flask import Flask, render_template
+
+app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
+
+@app.route('/')
+def hello():
+    count = get_hit_count()
+    # Render the index.html template and pass the hit count to it
+    return render_template('index.html', count=count)
